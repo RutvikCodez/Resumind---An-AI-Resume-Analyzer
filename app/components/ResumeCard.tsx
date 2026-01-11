9e5a77086c80a1fefba3636b43dcdbf93021a2a5
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-01-11 11:58:39 +0530

    landing page designed

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router";
+import ScoreCircle from "./ScoreCircle";
+
+const ResumeCard = ({
+  feedback,
+  id,
+  imagePath,
+  resumePath,
+  companyName,
+  jobTitle,
+  ...props
+}: Resume) => {
+  return (
+    <Link
+      to={`/resume/${id}`}
+      className="resume-card animate-in fade-in duration-100"
+      {...props}
+    >
+      <div className="resume-card-header">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-black font-bold wrap-break-word">
+            {companyName}
+          </h2>
+          <h3 className="text-lg wrap-break-word text-gray-500">{jobTitle}</h3>
+        </div>
+        <div className="shrink-0">
+          <ScoreCircle score={feedback.overallScore} />
+        </div>
+      </div>
+      <div className="gradient-border animate-in fade-in duration-100">
+        <div className="w-full h-full">
+            <img src={imagePath} alt="resume" className="w-full h-87.5 max-sm:h-50 object-cover object-top" />
+        </div>
+        
+      </div>
+    </Link>
+  );
+};
+
+export default ResumeCard;
