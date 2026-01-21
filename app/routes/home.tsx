e427692409b350c291b418ca19dc40b4520aac8a
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-01-22 01:25:41 +0530

    optimized code of home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -63,7 +63,7 @@ export default function Home() {
           </div>
         )}
         {!loadingResumes && resumes?.length === 0 && (
-          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+          <div className="flex flex-col items-center justify-center gap-4">
             <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
               Upload Resume
             </Link>
